d89003d8fa471d52040d635cf359a7d71cc61f48
singhpratyush <singh.pratyush96@gmail.com>
2017-05-03 14:50:23 +0530

    Add utility to build shop index for faster lookups

--- index.py
+++ index.py
@@ -1,28 +1,29 @@
-def build_index(data: list) -> dict:
+def build_index(data: list) -> tuple:
     """
-    Create an index with items for faster lookup. Format -
+    Create an index with items and shops for faster lookup. Format -
     {
-        'product1': {
-            'shops': set([1, 3, 5]),
-             'price': {
-                1: 10.0,
-                3: 11.5,
-                4: 8.0
-             }
-        },
+        'product1': 'shops': set([1, 3, 5]),
+        ...
+    }
+    {
+        shop1: [
+            (price1, [item1, item2, ...]),
+            ...
+        ],
         ...
     }
     :param data: List of tuples containg data about products and shops
-    :return: An index based on products
+    :return: An index based on products and shops
     """
-    index = {}  # Create an empty index
+    item_index = {}  # Create an empty item_index
+    shop_index = {}
     for item in data:
         for product in item[2]:
-            if product not in index:  # New, initialize
-                index[product] = {
-                    'shops': set(),
-                    'price': {}
-                }
-            index[product]['shops'].add(item[0])  # Add to shop set
-            index[product]['price'][item[0]] = item[1]  # Add price details
-    return index
+            if product not in item_index:  # New, initialize
+                item_index[product] = set()
+            item_index[product].add(item[0])  # Add to shop set
+
+        if item[0] not in shop_index:
+            shop_index[item[0]] = []
+        shop_index[item[0]].append((item[1], item[2:]))
+    return item_index, shop_index
